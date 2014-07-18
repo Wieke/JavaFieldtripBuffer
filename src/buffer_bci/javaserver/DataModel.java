a1a892a24d89060a63b06416861b9d9d544e1eee
Wieke Kanters <kantersw@gmail.com>
2014-07-18 14:14:04 +0200

    Added a datamodel abstract class.

---
+++ src/buffer_bci/javaserver/DataModel.java
@@ -0,0 +1,32 @@
+package buffer_bci.javaserver;
+
+import nl.fcdonders.fieldtrip.BufferEvent;
+import nl.fcdonders.fieldtrip.Header;
+
+public abstract class DataModel {
+
+	public abstract void putHeader(Header hdr);
+	
+	public abstract Header getHeader();
+	
+	public abstract void flushHeader();
+	
+	public abstract void putData();
+	
+	public abstract void getData();
+	
+	public abstract void flushData();
+	
+	public abstract void putEvent(BufferEvent event);
+	
+	public abstract BufferEvent[] getEvent(int begin, int end);
+	
+	public abstract void flushEvents();
+	
+	public abstract void getSampleCount();
+	
+	public abstract void getEventCount();
+	
+	public abstract void addPollListener();
+	
+}
