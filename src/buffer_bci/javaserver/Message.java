dc5b8bae9baf1aef1718344dca7c5372ce22d80f
Wieke Kanters <kantersw@gmail.com>
2014-07-18 13:45:53 +0200

    Added Message class.

---
+++ src/buffer_bci/javaserver/Message.java
@@ -0,0 +1,23 @@
+package buffer_bci.javaserver;
+
+import java.nio.ByteBuffer;
+
+public class Message {
+	public short version;
+	public short type;
+	public ByteBuffer buffer;
+	
+	public Message(short version, short type, ByteBuffer buffer){
+		this.version = version;
+		this.type = type;
+		this.buffer = buffer;
+	}
+	
+	@Override
+	public String toString() {
+	    return "(Version " + Short.toString(version) 
+	    		+ ", Type " + Short.toString(type) 
+	    		+ ", Size " + Integer.toString(buffer.capacity())
+	    		+ ")";
+	} 
+}
