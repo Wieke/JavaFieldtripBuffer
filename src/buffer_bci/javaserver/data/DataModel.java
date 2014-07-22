2b1605e083d5ee6725690ff87a6485ddad3a641c
Wieke Kanters <kantersw@gmail.com>
2014-07-22 16:54:32 +0200

    Added some comment/doc to the DataModel.

--- src/buffer_bci/javaserver/data/DataModel.java
+++ src/buffer_bci/javaserver/data/DataModel.java
@@ -7,31 +7,113 @@ import buffer_bci.javaserver.network.WaitRequest;
 
 public abstract class DataModel {
 
-	public abstract void addWaitListener(ServerThread listener,
+	/**
+	 * Adds a thread, with corresponding request, to the list of listeners of
+	 * this dataStore. Once the threshold, as defined in request, had been met
+	 * the threads waitOver() function will be called.
+	 * 
+	 * @param thread
+	 * @param request
+	 */
+	public abstract void addWaitListener(ServerThread thread,
 			WaitRequest request);
 
+	/**
+	 * Removes all data.
+	 * 
+	 * @throws DataException
+	 */
 	public abstract void flushData() throws DataException;
 
+	/**
+	 * Removes all events.
+	 * 
+	 * @throws DataException
+	 */
 	public abstract void flushEvents() throws DataException;
 
+	/**
+	 * Removes the header, and all data & events.
+	 * 
+	 * @throws DataException
+	 */
 	public abstract void flushHeader() throws DataException;
 
+	/**
+	 * Returns the requested data. Throws DataException if impossible.
+	 * 
+	 * @param request
+	 *            Start index and end index of the range requested.
+	 * @return
+	 * @throws DataException
+	 */
 	public abstract Data getData(Request request) throws DataException;
 
+	/**
+	 * Returns the number of events currently stored. Throws DataException if
+	 * impossible.
+	 * 
+	 * @return
+	 * @throws DataException
+	 */
 	public abstract int getEventCount() throws DataException;
 
+	/**
+	 * Returns the requested events. Throws DataException if impossible.
+	 * 
+	 * @param request
+	 *            Start index and end index of the range requested.
+	 * @return
+	 * @throws DataException
+	 */
 	public abstract Event[] getEvents(Request request) throws DataException;
 
+	/**
+	 * Returns the header currently stored. Throws DataException if impossible.
+	 * 
+	 * @return
+	 * @throws DataException
+	 */
 	public abstract Header getHeader() throws DataException;
 
+	/**
+	 * Returns the number of currently stored samples. Throws DataException if
+	 * impossible.
+	 * 
+	 * @return
+	 * @throws DataException
+	 */
 	public abstract int getSampleCount() throws DataException;
 
+	/**
+	 * Appends the data to the storage. Throws DataException if impossible.
+	 * 
+	 * @param data
+	 * @throws DataException
+	 */
 	public abstract void putData(Data data) throws DataException;
 
+	/**
+	 * Appends the events to the storage. Throws DataException if impossible.
+	 * 
+	 * @param events
+	 * @throws DataException
+	 */
 	public abstract void putEvents(Event[] events) throws DataException;
 
+	/**
+	 * Adds the header to the storage. Throws DataException if impossible.
+	 * 
+	 * @param hdr
+	 * @throws DataException
+	 */
 	public abstract void putHeader(Header hdr) throws DataException;
 
+	/**
+	 * Removes a thread from the list of listeners.
+	 * 
+	 * @param listener
+	 */
 	public abstract void removeWaitListener(ServerThread listener);
 
 }
