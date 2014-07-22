5c78260b7b64c5ca3d1f8081ea5d5f47eb3306ff
Wieke Kanters <kantersw@gmail.com>
2014-07-22 14:22:08 +0200

    Updated header to include chunks and byteorder. Added extra constructors.

--- src/buffer_bci/javaserver/data/Header.java
+++ src/buffer_bci/javaserver/data/Header.java
@@ -1,7 +1,13 @@
 package buffer_bci.javaserver.data;
 
-import buffer_bci.javaserver.exceptions.DataException;
+import java.nio.ByteOrder;
 
+/**
+ * Wrapper class for header information.
+ * 
+ * @author lkanters
+ * 
+ */
 public class Header {
 	public final int dataType;
 	public final float fSample;
@@ -10,8 +16,21 @@ public class Header {
 	public final int nEvents;
 	public final Chunk[] chunks;
 	public final int nChunks;
+	public final ByteOrder order;
 
-	public Header(int nChans, float fSample, int dataType) {
+	/**
+	 * Constructor
+	 * 
+	 * @param nChans
+	 *            Number of channels
+	 * @param fSample
+	 *            Sampling frequency
+	 * @param dataType
+	 *            Datatype of the data
+	 * @param order
+	 *            Bytorder of the data
+	 */
+	public Header(int nChans, float fSample, int dataType, ByteOrder order) {
 		this.nChans = nChans;
 		this.fSample = fSample;
 		nSamples = 0;
@@ -19,10 +38,25 @@ public class Header {
 		this.dataType = dataType;
 		chunks = null;
 		nChunks = 0;
+		this.order = order;
 	}
 
-	public Header(int nChans, float fSample, int dataType, Chunk[] chunks)
-			throws DataException {
+	/**
+	 * Constructor
+	 * 
+	 * @param nChans
+	 *            Number of channels
+	 * @param fSample
+	 *            Sampling frequency
+	 * @param dataType
+	 *            Datatype of the data
+	 * @param chunks
+	 *            Extended header chunks
+	 * @param order
+	 *            Bytorder of the data
+	 */
+	public Header(int nChans, float fSample, int dataType, Chunk[] chunks,
+			ByteOrder order) {
 		this.nChans = nChans;
 		this.fSample = fSample;
 		nSamples = 0;
@@ -30,5 +64,64 @@ public class Header {
 		this.dataType = dataType;
 		this.chunks = chunks;
 		nChunks = chunks.length;
+		this.order = order;
+	}
+
+	/**
+	 * Constructor
+	 * 
+	 * @param nChans
+	 *            Number of channels
+	 * @param nSamples
+	 *            Number of samples
+	 * @param nEvents
+	 *            Number of events
+	 * @param fSample
+	 *            Sampling frequency
+	 * @param dataType
+	 *            Datatype of the data
+	 * @param order
+	 *            Bytorder of the data
+	 */
+	public Header(int nChans, int nSamples, int nEvents, int fSample,
+			int dataType, ByteOrder order) {
+		this.nChans = nChans;
+		this.fSample = fSample;
+		this.nSamples = nSamples;
+		this.nEvents = nEvents;
+		this.dataType = dataType;
+		chunks = null;
+		nChunks = 0;
+		this.order = order;
+	}
+
+	/**
+	 * Constructor
+	 * 
+	 * @param nChans
+	 *            Number of channels
+	 * @param nSamples
+	 *            Number of samples
+	 * @param nEvents
+	 *            Number of events
+	 * @param fSample
+	 *            Sampling frequency
+	 * @param dataType
+	 *            Datatype of the data
+	 * @param chunks
+	 *            Extended header chunks
+	 * @param order
+	 *            Bytorder of the data
+	 */
+	public Header(int nChans, int nSamples, int nEvents, int fSample,
+			int dataType, Chunk[] chunks, ByteOrder order) {
+		this.nChans = nChans;
+		this.fSample = fSample;
+		this.nSamples = nSamples;
+		this.nEvents = nEvents;
+		this.dataType = dataType;
+		this.chunks = chunks;
+		nChunks = chunks.length;
+		this.order = order;
 	}
 }
