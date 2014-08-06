7b7083bf4b93c0d0f87ad7a4077eeb7fbe58b280
Wieke Kanters <kantersw@gmail.com>
2014-08-06 11:07:04 +0200

    Added extra error type.

--- src/buffer_bci/javaserver/FieldtripBufferMonitor.java
+++ src/buffer_bci/javaserver/FieldtripBufferMonitor.java
@@ -1,8 +1,13 @@
 package buffer_bci.javaserver;
 
 public interface FieldtripBufferMonitor {
+	public static final int ERROR_PROTOCOL = 0;
+	public static final int ERROR_CONNECTION = 1;
+	public static final int ERROR_VERSION = 2;
 
 	public void updateClientActivity(int clientID, long time);
+
+	public void updateClientError(int clientID, int errorType, long time);
 
 	public void updateConnectionClosed(int clientID);
 
